2f6807efc05d302ca7a76600563e1627964d6bfe
khan suhad <suhadahmodkhan@gmail.com>
2023-11-05 22:21:34 +0600

    added post operation in add jobs

--- index.js
+++ index.js
@@ -1,15 +1,67 @@
 const express = require('express');
 const cors = require('cors');
-const port = process.env.PORT || 5000 ;
+require('dotenv').config();
 const app = express();
+const port = process.env.PORT || 5000 ;
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/' , (req , res) => {
-    res.send("data is coming soon");
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.5y6t7ws.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+   
+    await client.connect();
+
+    const jobCollection = client.db("a11DB").collection("jobs")
+
+    app.get('/jobs/catagory/:catagory' , async(req , res) => {
+      const catagory = req.params.catagory ;
+      console.log(catagory)
+      const filter = { catagory : catagory};
+      const cursor = jobCollection.find(filter);
+      const result = await cursor.toArray();
+      res.send(result);
+  })
+
+    app.get('/jobs', async(req , res) => {
+        const cursor = jobCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.post('/jobs', async(req, res) => {
+        const job = req.body ;
+        const result = await jobCollection.insertOne(job)
+        res.send(result);
+        
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req , res) => {
+    res.send('daata is coming soon....')
 })
 
 app.listen(port , (req , res) => {
-    console.log('database is running succesfully ')
+    console.log(`database is running succesfully ${port}`)
 })
